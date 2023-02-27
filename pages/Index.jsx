2b048cf332f6d31a275d9066abc2a7b0012fbd08
Avaer Kazmer <a@modules.io>
2023-02-27 14:09:41 -0500

    Updaet main page again

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   }, []);
 
   return (
-    <pre>{baseUrl}api/?u=URL_TO_COMPILE</pre>
+    <pre>{baseUrl}api/compile?u=URL_TO_COMPILE</pre>
   );
 };
 export const Index = () => {
