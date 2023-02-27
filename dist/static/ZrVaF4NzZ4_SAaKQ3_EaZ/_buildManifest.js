9edf0900c50915e478de8a444cc3e28c42fe0428
Avaer Kazmer <a@modules.io>
2023-02-27 13:44:18 -0500

    Remove old dist files

--- dist/static/ZrVaF4NzZ4_SAaKQ3_EaZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/api/:path*"}],fallback:[]},"/":["static/chunks/pages/index-e3d15459d139ed31.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
